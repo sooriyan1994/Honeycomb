c5aaae4fcf2dae47f13044eba34cc4c9842f8e73
sooriyan1994 <45000714+sooriyan1994@users.noreply.github.com>
2019-06-12 19:05:29 +0530

    Add files via upload

--- dist_bw_pts.py
+++ dist_bw_pts.py
@@ -1,29 +1,24 @@
-import math
+import numpy as np
 import random
 import matplotlib.pyplot as plt
 
 def distance(X1,Y1):
     if isinstance(X1,list) and isinstance(Y1, list):
-        dis = math.sqrt((X1[0]-Y1[0])**2+(X1[1]-Y1[1])**2)
+        dis = np.sqrt((X1[0]-Y1[0])**2+(X1[1]-Y1[1])**2)
     else:
         print('ERROR. The inputs have to be of type "list"')
     return round(dis,4)
 
 def new_point(mag, cp):
     n1 = round(random.uniform(-1,1),4)
-    n2 = round(math.sqrt(1 - n1**2),4)
-    if random.randint(0,1) == 0:
-        x = cp[0] + (mag*n1)
-        y = cp[1] + (mag*n2)
-    else:
-        x = cp[0] + (mag*n2)
-        y = cp[1] + (mag*n1)
-    np = [round(x,4),round(y,4)]
-    return np
+    n2 = random.choice([-1,1]) * round(np.sqrt(1 - n1**2),4)
+    x = cp[0] + (mag*n1)
+    y = cp[1] + (mag*n2)
+    return [round(x,4),round(y,4)]
 
 def border(sq_side):
     border = []
-    deviation = [x * 0.1 for x in range(-sq_side*5,sq_side*5+1)]
+    deviation = list(np.arange(-0.5*sq_side, 0.5*sq_side + 0.1, 0.1))
     
     for i in range(4):
         for j in range(len(deviation)):
@@ -42,7 +37,3 @@ def coord_plot(coord):
     plt.scatter(X,Y)
     plt.show()
 
-def start_point(border, d):
-    cp = border[random.randint(0, len(border)-1)]
-    print cp
-    return new_point(d, cp)
